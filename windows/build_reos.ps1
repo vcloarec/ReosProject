c81da44932c38c1ced7f0b20170645be7b7338a0
vcloarec <vcloarec@gmail.com>
2022-12-18 00:16:16 -0400

    CI: fix dss lib path for cmake

--- windows/build_reos.ps1
+++ windows/build_reos.ps1
@@ -82,8 +82,8 @@ cmake   -S $env:REOS_SOURCE `
         -D QWT_LIB=$OSGEO_DIR/apps/Qt5/lib/qwt.lib `
         -D WITH_QTWEBKIT:BOOL=FALSE `
         -D ENABLE_HEC_DSS=TRUE `
-        -D HEC_DSS_INCLUDE=$env:REOS_SOURCE/external/heclib_win/heclib-7-IP-10-win-x86_64/headers
-        -D HEC_DSS_LIBS_PATH=$env:REOS_SOURCE/external/heclib_win/heclib-7-IP-10-win-x86_64/Release64
+        -D HEC_DSS_INCLUDE=$env:REOS_SOURCE/external/heclib_win/heclib-7-IP-10-win-x86_64/headers `
+        -D HEC_DSS_LIBS_PATH=$env:REOS_SOURCE/external/heclib_win/heclib-7-IP-10-win-x86_64/Release64 `
         -D INTEL_FORTRAN_COMPILER_LIB_PATH=$env:FORTRAN_COMPILER_PATH\windows\compiler\lib\intel64_win
 
 cmake --build .  --config $env:BUILD_TYPE --parallel
