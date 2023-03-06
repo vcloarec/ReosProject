6f0b7cd88543f1ea2144acb61d29f90a345cd65b
vcloarec <vcloarec@gmail.com>
2023-03-06 17:19:32 -0400

    topographie  = 0 when no dems

--- src/core/GIS/private/reostopographycollection_p.cpp
+++ src/core/GIS/private/reostopographycollection_p.cpp
@@ -38,18 +38,21 @@ void ReosTopographyCollection_p::prepare_p( const QgsCoordinateReferenceSystem &
     std::unique_ptr<ReosDigitalElevationModel> dem( mGisEngine->getDigitalElevationModel( layerId ) );
     if ( dynamic_cast<ReosDigitalElevationModelRaster *>( dem.get() ) )
     {
-      mDem.emplace_back( dynamic_cast<ReosDigitalElevationModelRaster *>( dem.release() ) );
-      mTransforms.append( mDem.back()->transformToDem( sourceCrs ) );
+      mDems.emplace_back( dynamic_cast<ReosDigitalElevationModelRaster *>( dem.release() ) );
+      mTransforms.append( mDems.back()->transformToDem( sourceCrs ) );
     }
   }
 }
 
 double ReosTopographyCollection_p::elevationAt_p( const QgsPointXY &point ) const
 {
-  for ( size_t i = 0; i < mDem.size(); ++i )
+  if ( mDems.empty() )
+    return 0;
+
+  for ( size_t i = 0; i < mDems.size(); ++i )
   {
-    double value = mDem.at( i )->elevationAt( point, mTransforms.at( int( i ) ) );
-    if ( value != mDem.at( i )->noDataValue() && !std::isnan( value ) )
+    double value = mDems.at( i )->elevationAt( point, mTransforms.at( int( i ) ) );
+    if ( value != mDems.at( i )->noDataValue() && !std::isnan( value ) )
       return value;
   }
 
@@ -58,6 +61,6 @@ double ReosTopographyCollection_p::elevationAt_p( const QgsPointXY &point ) cons
 
 void ReosTopographyCollection_p::clean_p() const
 {
-  mDem.clear();
+  mDems.clear();
   mTransforms.clear();
 }
