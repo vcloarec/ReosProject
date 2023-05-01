bbc4f6e4e6f68c6678d4a3ead2cd239b68c3961f
vcloarec <vcloarec@gmail.com>
2023-04-30 23:44:07 -0400

    SIPIFY

---
+++ python/core/auto_additions/reoshydraulicstructure2d.py
@@ -0,0 +1,4 @@
+# The following has been generated automatically from src/core/hydraulicNetwork/reoshydraulicstructure2d.h
+ReosHydraulicStructure2D.Structure2DCapability.baseClass = ReosHydraulicStructure2D
+ReosHydraulicStructure2D.Structure2DCapabilities.baseClass = ReosHydraulicStructure2D
+Structure2DCapabilities = ReosHydraulicStructure2D  # dirty hack since SIP seems to introduce the flags in module
