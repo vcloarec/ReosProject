2dbe526866088a3e4b05ff9ff49d223f507347c6
vcloarec <vcloarec@gmail.com>
2023-07-17 13:50:26 +0200

    qgis prefix path for docker

--- docker/pyReos_interpreter.sh
+++ docker/pyReos_interpreter.sh
@@ -1,6 +1,7 @@
 export LD_LIBRARY_PATH=$REOS_DIR/lib:/usr/lib/x86_64-linux-gnu/:$GMSH_DIR/lib/:$QGIS_DIR/lib/:$MDAL_DIR/lib/
 export PYTHONPATH="$REOS_DIR/python/":$PYTHONPATH
 export QGIS_PLUGIN_PATH=$QGIS_DIR/lib/qgis/plugins
+export QGIS_PREFIX_PATH=$QGIS_DIR
 export REOS_PREFIX_PATH=$REOS_DIR
 
-python3
+python3 $1
