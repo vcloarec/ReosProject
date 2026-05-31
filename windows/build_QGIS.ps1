9464d6f3e801b017df9b5f768c7bf0c22c3da2cf
vcloarec <vcloarec@gmail.com>
2026-06-01 00:00:02 +0200

    fix: point GSL include and libraries to OSGeo paths in Windows build script
    CMake should use the GSL headers and libraries provided under the OSGeo distribution. Update windows/build_QGIS.ps1 to set GSL_INCLUDE_DIR to $OSGEO_DIR/include/gsl and to reference the OSGeo GSL and CBLAS libraries ($OSGEO_DIR/lib/gsl.lib and $OSGEO_DIR/lib/cblas.lib) so the Windows CMake configuration picks up the correct headers and libs.

--- windows/build_QGIS.ps1
+++ windows/build_QGIS.ps1
@@ -58,9 +58,9 @@ cmake   -S $env:QGIS_SRC `
 		-D GDAL_INCLUDE_DIR=$env:GDAL_ROOT/include `
 		-D PROJ_INCLUDE_DIR=$OSGEO_DIR/include `
 		-D GEOS_LIBRARY=$OSGEO_DIR/lib/geos_c.lib `
-		-D GSL_INCLUDE_DIR=$GSL_INCLUDE_DIR/gsl `
-        -D GSL_LIBRARY=$GSL_LIBRARY `
-        -D GSL_CBLAS_LIBRARY=$GSL_CBLAS_LIBRARY `
+		-D GSL_INCLUDE_DIR=$OSGEO_DIR/include/gsl `
+        -D GSL_LIBRARY=$OSGEO_DIR/lib/gsl.lib `
+        -D GSL_CBLAS_LIBRARY=$OSGEO_DIR/lib/cblas.lib `
 		-D SQLITE3_LIBRARY=$OSGEO_DIR/lib/sqlite3_i.lib `
 		-D SPATIALITE_LIBRARY=$OSGEO_DIR/lib/spatialite_i.lib `
 		-D SPATIALINDEX_LIBRARY=$OSGEO_DIR/lib/spatialindex-64.lib `
