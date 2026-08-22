1f990be115ad52d4e21c573c45cb728b49ecb4af
vcloarec <vcloarec@gmail.com>
2026-08-22 17:43:59 +0200

    Run SIP build with verbose output (--verbose)
    Add the --verbose flag to the SIP invocation so SIP emits more detailed output during code generation. This makes failures and diagnostics from the SIP build easier to see when running the CMake build.

--- cmake/SIPMacros.cmake
+++ cmake/SIPMacros.cmake
@@ -117,7 +117,7 @@ MACRO(GENERATE_SIP_PYTHON_MODULE_CODE MODULE_NAME MODULE_SIP SIP_FILES CPP_FILES
       SET(_sip_build_env ${CMAKE_COMMAND} -E env "PATH=${_sip_build_path}")
     ENDIF(QMAKE_EXECUTABLE)
 
-    SET(SIPCMD ${_sip_build_env} ${SIP_BUILD_EXECUTABLE} --no-protected-is-public --pep484-pyi --no-compile --concatenate=${SIP_CONCAT_PARTS} --include-dir=${CMAKE_CURRENT_BINARY_DIR} --include-dir=${PYQT_SIP_DIR} --api-dir ${CMAKE_BINARY_DIR}/python ${SIP_BUILD_EXTRA_OPTIONS})
+    SET(SIPCMD ${_sip_build_env} ${SIP_BUILD_EXECUTABLE} --verbose --no-protected-is-public --pep484-pyi --no-compile --concatenate=${SIP_CONCAT_PARTS} --include-dir=${CMAKE_CURRENT_BINARY_DIR} --include-dir=${PYQT_SIP_DIR} --api-dir ${CMAKE_BINARY_DIR}/python ${SIP_BUILD_EXTRA_OPTIONS})
 
     ADD_CUSTOM_COMMAND(
       OUTPUT ${_sip_output_files}
